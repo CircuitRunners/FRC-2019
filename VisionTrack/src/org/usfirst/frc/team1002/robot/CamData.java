785e4962d37ee17ae1e1e1b1f64712c33a3e07be
NathanPendergrast <nathanpendergrast@yahoo.com>
2018-11-19 02:01:31 -0500

    FORK YEA VISION TRACKING

--- VisionTrack/src/org/usfirst/frc/team1002/robot/CamData.java
+++ VisionTrack/src/org/usfirst/frc/team1002/robot/CamData.java
@@ -2,9 +2,9 @@ package org.usfirst.frc.team1002.robot;
 
 public class CamData {
 
-public static final double verticalFOV = 34.3;
-public static final double horizontalFOV = 61.0;
-public static final double diagonalFOV = 68.5;
+public static final double verticalFOV = 33.583;//half of the FOV (center to edge)
+public static final double horizontalFOV = 59.703;//half of the FOV (center to edge)
+public static final double diagonalFOV = 68.5;//used math to find the first two from this, this was on the LIFECAM HD-3000 product page
 
 
 }
--- VisionTrack/src/org/usfirst/frc/team1002/robot/Camera.java
+++ VisionTrack/src/org/usfirst/frc/team1002/robot/Camera.java
@@ -1,6 +1,13 @@
 package org.usfirst.frc.team1002.robot;
 
+import java.util.ArrayList;
+
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
@@ -12,8 +19,10 @@ public class Camera {
 	private UsbCamera mainCam;
 	private CvSink main;
 	public int frameNumber = 0;
+	private int frameWidth = 640;
+	private int frameHeight = 480;
 	private static double frameDelay = 0.05;
-	
+	private static ArrayList<MatOfPoint> contours;
 	public boolean imageTracking = false;
 
 	public void init() {
@@ -22,33 +31,66 @@ public class Camera {
 				cameraOperation();
 			}
 		});
+		cameraOpThread.setName("CamThread");
+		cameraOpThread.start();
 	}
 
 	protected void cameraOperation() {
 		frameNumber = 0;
-		
+		GripPipeline gripProcessor = new GripPipeline();
+		boolean targetFound = false;
+		int numContours = 0;
+		int xCenter = 0;
+		double xAngle = 0;
+		int yCenter = 0;
+		double yAngle = 0;
+		MatOfPoint contour;
+		Rect box;
+
 		mainCam = CameraServer.getInstance().startAutomaticCapture("main", 0);// sets up camera
-		mainCam.setResolution(640, 480);
+		mainCam.setResolution(frameWidth, frameHeight);
 		mainCam.setFPS(30);
 		mainCam.setExposureHoldCurrent();
 		mainCam.setExposureManual(10);
 
 		main = CameraServer.getInstance().getVideo(mainCam);// sets video output
 
-		CvSource outputStream = CameraServer.getInstance().putVideo("DispWin", 640, 480);
+		CvSource outputStream = CameraServer.getInstance().putVideo("DispWin", frameWidth, frameHeight);
 
 		Mat camMat = new Mat();
 
 		while (!Thread.interrupted()) {
-			if(!imageTracking) {
-				outputStream.putFrame(camMat);	
+			if (!imageTracking) {
+				outputStream.putFrame(camMat);
 			} else {
 				System.out.println("Camera is now vision tracking!");
-				
+				targetFound = false;
+				gripProcessor.process(camMat);
+				contours = gripProcessor.filterContoursOutput();
+				numContours = contours.size();
+				if (numContours > 1) {
+					numContours = 1;
+				}
+				xCenter = yCenter = 0;
+				if (numContours > 0) {
+					for (int i = 0; i < numContours; i++) {
+						contour = contours.get(i);
+						box = Imgproc.boundingRect(contour);
+						xCenter = box.x + (box.width / 2);
+						yCenter = box.y + (box.height / 2);
+					}
+					xCenter /= numContours;
+					yCenter /= numContours;
+					
+					xAngle = ((xCenter - (frameWidth / 2)) / (frameWidth / 2)) * CamData.horizontalFOV;
+					yAngle = ((yCenter - (frameWidth / 2)) / (frameWidth / 2)) * CamData.verticalFOV;
+					
+					Imgproc.circle(camMat, new Point(xCenter, yCenter), 15, new Scalar(235, 55, 15), 2);
+					targetFound = true;
+				}
 			}
 			Timer.delay(frameDelay);
 		}
 	}
-	
-	
+
 }
