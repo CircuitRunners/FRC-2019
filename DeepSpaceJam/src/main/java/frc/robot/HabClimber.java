0f29646eecc146be4523087673287d2ba6616363
NathanPendergrast <nathanpendergrast@yahoo.com>
2019-01-22 08:28:19 -0500

    set up the Hab Climber

--- DeepSpaceJam/src/main/java/frc/robot/HabClimber.java
+++ DeepSpaceJam/src/main/java/frc/robot/HabClimber.java
@@ -3,10 +3,10 @@ package frc.robot;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class HabClimber{
-    DoubleSolenoid frontLeft;
-    DoubleSolenoid frontRight;
-    DoubleSolenoid backLeft;
-    DoubleSolenoid backRight;
+    static DoubleSolenoid frontLeft;
+    static DoubleSolenoid frontRight;
+    static DoubleSolenoid backLeft;
+    static DoubleSolenoid backRight;
 
     public void init(){
         frontLeft = new DoubleSolenoid(1, 2);
@@ -14,5 +14,21 @@ public class HabClimber{
         backLeft = new DoubleSolenoid(5,6);
         backRight = new DoubleSolenoid(7,8);
 
+    }
+    public static void raise(boolean endGame){
+        if(endGame){
+            frontLeft.set(DoubleSolenoid.Value.kForward);
+            backLeft.set(DoubleSolenoid.Value.kForward);
+            frontRight.set(DoubleSolenoid.Value.kForward);
+            backRight.set(DoubleSolenoid.Value.kForward);
+        } else {
+            System.out.println("Not Endgame Yet!!!");
+        }
+    }
+    public static void lower(){
+        frontLeft.set(DoubleSolenoid.Value.kReverse);
+        backLeft.set(DoubleSolenoid.Value.kReverse);
+        frontRight.set(DoubleSolenoid.Value.kReverse);
+        backRight.set(DoubleSolenoid.Value.kReverse); 
     }
 }
