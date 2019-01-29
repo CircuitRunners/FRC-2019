0588b8d9b459d5d8fbf3c04880d822b9a9a2e237
NathanPendergrast <nathanpendergrast@yahoo.com>
2019-01-28 19:27:43 -0500

    more work on the elevator class

--- DeepSpaceJam/src/main/java/frc/robot/Elevator.java
+++ DeepSpaceJam/src/main/java/frc/robot/Elevator.java
@@ -7,14 +7,62 @@
 
 package frc.robot;
 
-//import edu.wpi.first.wpilibj.Talon;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+//import edu.wpi.first.wpilibj.Talon;
 /**
  * Add your docs here.
  * 
  */
 public class Elevator {
-    TalonSRX elevator;
-    
-    
+    private static TalonSRX elevator;
+    public static void init(){
+        elevator = new TalonSRX(9);
+        elevator.clearStickyFaults();
+    }
+    public static void run(){
+        
+    }
+    int elevTimeoutMs = 10;
+    int elevPIDLoopIdx = 0;
+    int elevSlotIdx = 0;
+    int elevCV = 15000;
+    int maxElevCV = 18000;
+    int elevCA = 15000;
+    int maxElevCA = 18000;
+    public void talonConfig(TalonSRX thisTalon, boolean inverted) {
+		/* first choose the sensor */
+		thisTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, elevPIDLoopIdx,
+				elevTimeoutMs);
+		thisTalon.setSensorPhase(true);
+		thisTalon.setInverted(false);
+
+		/* Set relevant frame periods to be at least as fast as periodic rate */
+		thisTalon.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, elevTimeoutMs);
+		thisTalon.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, elevTimeoutMs);
+		thisTalon.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 10, elevTimeoutMs);
+		/* set the peak and nominal outputs */
+		thisTalon.configNominalOutputForward(0, elevTimeoutMs);
+		thisTalon.configNominalOutputReverse(0, elevTimeoutMs);
+		thisTalon.configPeakOutputForward(1, elevTimeoutMs);
+		thisTalon.configPeakOutputReverse(-1, elevTimeoutMs);
+
+		/* set closed loop gains in slot0 - see documentation */
+		thisTalon.selectProfileSlot(elevSlotIdx, elevPIDLoopIdx);
+		thisTalon.config_kF(0, 0.287, elevTimeoutMs);
+		thisTalon.config_kP(0, 0.4, elevTimeoutMs);
+		thisTalon.config_kI(0, 0, elevTimeoutMs);
+		thisTalon.config_kD(0, 0, elevTimeoutMs);
+		/* set acceleration and vcruise velocity - see documentation */
+		thisTalon.configMotionCruiseVelocity(elevCV, elevTimeoutMs);
+		thisTalon.configMotionAcceleration(elevCV, elevTimeoutMs);
+		/* zero the sensor */
+		thisTalon.setSelectedSensorPosition(0, elevPIDLoopIdx, elevTimeoutMs);
+
+		//thisTalon.configMotionCruiseVelocity((int) RobotData.elevCruiseVel, elevTimeoutMs);
+		//thisTalon.configMotionAcceleration((int) RobotData.elevCruiseAccel, elevTimeoutMs);
+
+	}
 }
